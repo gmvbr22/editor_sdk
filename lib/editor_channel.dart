eb8ccf3b1d44857360b1aa548ca0d9afc55fbdca
gmvbr <87571591+gmvbr@users.noreply.github.com>
2022-01-20 08:35:10 -0400

    Update editor_channel

--- lib/editor_channel.dart
+++ lib/editor_channel.dart
@@ -1,18 +1,10 @@
 import 'package:editor_sdk/editor_sdk.dart';
 
 abstract class EditorChannel {
-  ///
-  /// Save type to json
-  ///
-  void saveToJson(EditorType type);
+ 
+  dynamic saveToJson(EditorType type);
 
-  ///
-  /// Load type from json
-  ///
   EditorType loadFromJson(dynamic json);
 }
 
-///
-/// EditorChannel singleton
-///
 late EditorChannel editorChannel;
